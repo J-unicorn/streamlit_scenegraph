17e2e5e7fb24cd66e2b06dc66a363beec63b8dd0
Doohee Jung <98963763+J-unicorn@users.noreply.github.com>
2023-03-16 09:43:28 +0900

    Update III. Scene Graph Generation.py

--- pages/III. Scene Graph Generation.py
+++ pages/III. Scene Graph Generation.py
@@ -155,8 +155,8 @@ def Practice():
         if image_file:
             st.markdown("#### SceneGraph 생성을 해보세요.")
             pred_button = st.button("Scene Graph Detection")
-            org_image = Image.open(image_file, mode='r').convert('RGB')
-            st.image(org_image, caption='Upload Image')
+           # org_image = Image.open(image_file, mode='r').convert('RGB')
+          #  st.image(org_image, caption='Upload Image')
             df = image_extraction(image_file)
             if pred_button:
                 st.session_state.predbtn_state = True
